5dbc083e3ea4693b4c0da6c2ec352cb573fb3b24
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-26 11:52:51 +0300

    DONE

--- main.js
+++ main.js
@@ -3,10 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
-
+const articleHearts = document.querySelectorAll(".like-glyph");
+function likeCallback(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(() => {
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(error => {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+for (const glyph of articleHearts) {
+  // glyph.addEventListener("click", heart);
+  glyph.addEventListener("click", likeCallback);
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
